8f74e2278b82b25093604a84787e6ea731884d39
mzgd <mzgd@MZNB170008.mz.gpgc.local>
2018-07-29 09:52:27 +0800

    连接数据库完成

---
+++ src/cn/dw/utils/JdbcUtils.java
@@ -0,0 +1,35 @@
+package cn.dw.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Date;
+
+public class JdbcUtils {
+	static {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			throw new RuntimeException(e);
+		}
+	}
+	
+	
+	public Connection getConnection() {
+		try {
+			return DriverManager.getConnection("jdbc:mysql://localhost:3306/studyJava", "root", "123456");
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+		JdbcUtils utils = new JdbcUtils();
+		Connection connection = utils.getConnection();
+		System.out.println(connection);
+		new Date();
+	}
+
+}
